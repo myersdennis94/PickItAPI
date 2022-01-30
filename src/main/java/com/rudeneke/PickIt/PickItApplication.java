18ff972ca67f43830ffb1b3c689260b605f460b1
myersdennis94 <myers.dennis.94@gmail.com>
2022-01-30 11:07:06 -0600

    Initial commit

---
+++ src/main/java/com/rudeneke/PickIt/PickItApplication.java
@@ -0,0 +1,13 @@
+package com.rudeneke.PickIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudeneke/PickIt/PickItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudeneke.PickIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
